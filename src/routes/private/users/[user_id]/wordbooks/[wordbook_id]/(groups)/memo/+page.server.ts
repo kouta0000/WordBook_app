80ff3851ed259ff593295f3d7935bcff8cd50a99
kouta0000 <39004878+kouta0000@users.noreply.github.com>
2025-05-23 20:52:54 +0900

    Update +page.server.ts

--- src/routes/private/users/[user_id]/wordbooks/[wordbook_id]/(groups)/memo/+page.server.ts
+++ src/routes/private/users/[user_id]/wordbooks/[wordbook_id]/(groups)/memo/+page.server.ts
@@ -2,9 +2,11 @@ import {supabase} from "$lib/config/supabaseClient";
 import type {PageServerLoad} from "./$types";
 
 
-export const load: PageServerLoad = async ({params}) => {
+export const load: PageServerLoad = async ({params, cookies}) => {
     const wordbook_id: string = params.wordbook_id;
     const user_id: string = params.user_id;
+    cookies.set("wordbook_id", wordbook_id, {path: "/"});
+    cookies.set("user_id", user_id, {path: "/"});
     const words = await supabase.from("Words").select("term, meaning, id").eq("wb_id", wordbook_id).eq("user_id", user_id).order("created_at",{ascending:false});
     return words
 }
@@ -14,6 +16,8 @@ export const actions = {
         const data = await request.formData();
         const term: string = data.get("term");
         const meaning: string = data.get("meaning");
+        const wordbook_id: string = cookies.get("wordbook_id");
+        const user_id: string = cookies.get("user_id");
         const {error} = await supabase.from("Words").insert({"user_id": user_id, "wb_id": wordbook_id, "term": term, "meaning": meaning})
     },
     deleteWord: async ({ cookies, request }) => {
