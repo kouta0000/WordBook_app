aaabf8a51311670e7535a63e0fe5497f7240745f
kouta0000 <39004878+kouta0000@users.noreply.github.com>
2025-05-23 19:37:54 +0900

    Update +page.server.ts to fix bug

--- src/routes/private/users/[user_id]/wordbooks/[wordbook_id]/(groups)/memo/+page.server.ts
+++ src/routes/private/users/[user_id]/wordbooks/[wordbook_id]/(groups)/memo/+page.server.ts
@@ -1,12 +1,10 @@
 import {supabase} from "$lib/config/supabaseClient";
 import type {PageServerLoad} from "./$types";
 
-let wordbook_id: string;
-let user_id:string;
 
 export const load: PageServerLoad = async ({params}) => {
-    wordbook_id = params.wordbook_id;
-    user_id = params.user_id;
+    const wordbook_id: string = params.wordbook_id;
+    const user_id: string = params.user_id;
     const words = await supabase.from("Words").select("term, meaning, id").eq("wb_id", wordbook_id).eq("user_id", user_id).order("created_at",{ascending:false});
     return words
 }
