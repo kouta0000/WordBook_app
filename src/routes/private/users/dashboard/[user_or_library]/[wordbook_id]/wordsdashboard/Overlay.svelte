0570be348887a2529bda5110a197540a5d53234a
kouta0000 <39004878+kouta0000@users.noreply.github.com>
2025-05-31 03:40:52 +0900

    Update Overlay.svelte

--- src/routes/private/users/dashboard/[user_or_library]/[wordbook_id]/wordsdashboard/Overlay.svelte
+++ src/routes/private/users/dashboard/[user_or_library]/[wordbook_id]/wordsdashboard/Overlay.svelte
@@ -10,12 +10,12 @@
 {:else}
 <div transition:fade={{duration:150}} class={{
     "inset-0 rounded-3xl absolute z-8 flex transition-all duration-300":true,
-    "bg-sky-400": (i % 6 == 0),
-    "bg-sky-200 md:bg-sky-400": (i % 6 == 1),
-    "bg-sky-400 md:bg-sky-200 lg:bg-sky-400": (i % 6 == 2),
-    "bg-sky-200  ": (i % 6 == 3),
-    "bg-sky-400 lg:bg-sky-200": (i % 6 == 4),
-    "bg-sky-200 md:bg-sky-400 lg:bg-sky-200": (i % 6 == 5),
+    "bg-sky-200": (i % 6 == 0),
+    "bg-sky-100 md:bg-sky-200": (i % 6 == 1),
+    "bg-sky-200 md:bg-sky-100 lg:bg-sky-200": (i % 6 == 2),
+    "bg-sky-100  ": (i % 6 == 3),
+    "bg-sky-200 lg:bg-sky-100": (i % 6 == 4),
+    "bg-sky-100 md:bg-sky-200 lg:bg-sky-100": (i % 6 == 5),
     "opacity-0":isClicked
 }}  onclick={() => {isClicked = !isClicked; setTimeout(()=>isClicked = !isClicked, 2500)}}>
 <p class="m-auto text-white font-bold">show</p>
