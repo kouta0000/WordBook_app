914eeabada0241c917856899d5f712e83a80174b
kouta0000 <39004878+kouta0000@users.noreply.github.com>
2025-05-31 01:13:09 +0900

    Update Edit.svelte

--- src/routes/private/users/dashboard/[user_or_library]/[wordbook_id]/wordsdashboard/Edit.svelte
+++ src/routes/private/users/dashboard/[user_or_library]/[wordbook_id]/wordsdashboard/Edit.svelte
@@ -93,7 +93,7 @@
                         updatings2[id]=false;
                   }
                 }} class={{"absolute inset-0 flex flex-col justifry-center items-start z-12":true}}>
-                    <span  class="pl-2 pr-2 flex border-1 border-stone-300 shadow-lg rounded-xl max-w-9/10 -translate-x-3 translate-y-1 z-1 bg-white">
+                    <span  class="pl-2 pr-2 flex border-1 border-stone-300  rounded-xl max-w-9/10 -translate-x-3 translate-y-1 z-1 bg-white">
                         
                         <input type="text" id="term" name="term" value={word.term} class="rounded-3xl my-1 input m-auto  font-semibold font-sans text-xl">
                         <button type="button" onclick={()=>{isChecked=!isChecked;updatings[id] = false}} class="btn btn-base rounded-full my-auto btn-sm">
@@ -103,7 +103,7 @@
                             保存
                         </button>
                     </span>
-                    <div class="px-2 flex w-full shadow-lg border-1 border-stone-300 rounded-3xl relative bg-white">
+                    <div class="px-2 flex w-full border-stone-300 rounded-xl relative bg-white">
                         <input type="text" id="meaning" name="meaning" value={word.meaning} class={{"rounded-3xl my-3 py-2 input mx-auto max-w-9/10 font-sans text-lg":true}}>
                     </div>
                     
@@ -112,10 +112,10 @@
                 {/if}
                 {#if updatings2[id]}
                 <div class={{"absolute inset-0 flex flex-col justifry-center items-start z-12":true}}>
-                <span out:fade={{delay:1000}} class="pl-2 pr-2 flex border-1 border-stone-300 shadow-lg rounded-xl max-w-9/10 -translate-x-3 translate-y-1 z-1 skeleton">
+                <span out:fade={{delay:1000}} class="pl-2 pr-2 flex border-1 border-stone-300 rounded-xl max-w-9/10 -translate-x-3 translate-y-1 z-1 skeleton">
                     <p class="m-auto px-10 py-2 font-semibold font-sans text-xl opacity-0">{word.term}</p>
                 </span>
-                <div out:fade={{delay:1000}} class="px-2 flex w-full shadow-lg border-1 border-stone-300 rounded-3xl relative skeleton">
+                <div out:fade={{delay:1000}} class="px-2 flex w-full border-stone-300 rounded-xl relative skeleton">
                     <p class="mx-auto my-4 max-w-9/10 font-sans text-lg opacity-0">Loading...</p>
                 </div>
                 </div>
