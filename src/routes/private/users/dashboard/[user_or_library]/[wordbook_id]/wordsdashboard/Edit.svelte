f0c98d2deeadbd2b228df5f180e1142673884656
kouta0000 <39004878+kouta0000@users.noreply.github.com>
2025-05-31 03:24:53 +0900

    Update Edit.svelte

--- src/routes/private/users/dashboard/[user_or_library]/[wordbook_id]/wordsdashboard/Edit.svelte
+++ src/routes/private/users/dashboard/[user_or_library]/[wordbook_id]/wordsdashboard/Edit.svelte
@@ -156,7 +156,7 @@
                     </div>
                     {/if}         
                 </div>
-                <hr class="w-full scale-80 h-1 mt-4 bg-gray-200 border-0 rounded-sm md:my-10 dark:bg-gray-700">
+                <hr class="w-full scale-90 h-2 mt-4 bg-gray-300 border-0 rounded-sm md:my-10 dark:bg-gray-700">
             </div>
             
             {/each}
