c1ba1b33168022bfd441efc16c1e13f4bfa07ac0
kota <kukkmathphys1236@gmail.com>
2025-06-05 15:08:38 +0900

    ;;

--- src/routes/private/users/dashboard/[user_or_library]/[wordbook_id]/wordsdashboard/Edit.svelte
+++ src/routes/private/users/dashboard/[user_or_library]/[wordbook_id]/wordsdashboard/Edit.svelte
@@ -115,7 +115,7 @@
                 </form>
                 {/if}
                 {#if updatings2[id]}
-                <div class={{"absolute inset-0 flex flex-col justifry-center items-start z-1":true}}>
+                <div class={{"absolute inset-0 flex flex-col justifry-center items-start z-5":true}}>
                 <span out:fade={{delay:1000}} class="pl-2 pr-2 flex border-1 border-stone-300 rounded-xl max-w-9/10 -translate-x-3 translate-y-1 z-1 skeleton">
                     <p class="m-auto px-10 py-2 font-semibold font-sans text-xl opacity-0">{word.term}</p>
                 </span>
