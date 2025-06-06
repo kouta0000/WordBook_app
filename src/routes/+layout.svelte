8d6de8519feca26c9fdf7d514d0186c55b254d88
kouta0000 <39004878+kouta0000@users.noreply.github.com>
2025-06-06 13:39:19 +0900

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -17,10 +17,10 @@ onMount(() => {
     });
 </script>
 
-    <div class="h-18 w-full fixed absolute top-0 flex flex-row items-center bg-white border-b border-base-300 z-10">
-        <object data="/images/mv.svg" type="image/svg+xml" class="h-12 w-12 m-3">ロゴ</object>
+    <div class="h-15 w-full fixed absolute top-0 flex flex-row items-center bg-white border-b border-base-300 z-10">
+        <object data="/images/mv.svg" type="image/svg+xml" class="h-9 w-9 m-3">ロゴ</object>
         <span class="mx-auto"><h1 class="text-xl md:text-2xl font-serif font-bold">MonVocab</h1></span>
-        <div class="h-18 w-18"></div>
+        <div class="h-15 w-15"></div>
     </div>
 
 
