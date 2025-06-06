be5862f0ca415b8aed09d8d66cd1f5e21dea06c0
kouta0000 <39004878+kouta0000@users.noreply.github.com>
2025-06-06 13:47:14 +0900

    Update +page.svelte

--- src/routes/private/users/dashboard/[user_or_library]/[wordbook_id]/wordsdashboard/+page.svelte
+++ src/routes/private/users/dashboard/[user_or_library]/[wordbook_id]/wordsdashboard/+page.svelte
@@ -30,39 +30,27 @@
 <div class="w-full h-12 md:h-15 bg-white fixed absolute bottom-0 flex flex-row justify-center gap-2 z-20">
     {#if user_or_library == "user"}
     <button onclick={()=> currentView = "edit"} class={{
-        "btn btn-ghost font-bold h-15 md:h-20 grow-1 basis-0":true,
+        "btn btn-ghost font-bold h-full grow-1 basis-0":true,
         "text-sky-500":currentView =="edit",
-        "text-gray-500":currentView=="memo" || currentView=="test"
+        "text-gray-500":currentView=="memo" || currentView=="test" || currentView == "test2"
         }}>ホーム</button>
     {/if}
     <button onclick={()=> currentView = "memo"} class={{
-        "btn btn-ghost font-bold h-15 md:h-20 grow-1 basis-0":true,
+        "btn btn-ghost font-bold h-full grow-1 basis-0":true,
         "text-sky-500":currentView=="memo",
-        "text-gray-500": currentView=="test" || currentView == "edit"
+        "text-gray-500": currentView=="test" || currentView == "edit" || currentView == "test2"
         }}>覚える</button>
     
-    <button onclick={() => currentView = "test"} class="btn btn-ghost h-15 md:h-20 grow-1 basis-0">
-        <svg class="h-full" viewBox="0 0 40 40" fill="none">
-            <rect x="15" y="15" width="10" height="10" class={{
-                "fill-sky-500":currentView=="test",
-                "fill-gray-500": currentView=="memo" || currentView == "edit" || currentView=="test2"
-                }} />
-            <line x1="17" y1="20" x2="19" y2="23" class="stroke-1 rounded-2xl stroke-white" stroke-linecap="round"/>
-            <line x1="19" y1="23" x2="23" y2="17" class="stroke-1 rounded-2xl stroke-white" stroke-linecap="round"/>
-            <text x="15" y="35" class="fill-gray-500" font-size="6">4択</text>
-        </svg>
-    </button>
+    <button onclick={() => currentView = "test"}　class={{
+        "btn btn-ghost font-bold h-full 0 grow-1 basis-0":true,
+        "text-sky-500":currentView=="test",
+        "text-gray-500": currentView=="memo" || currentView == "edit" || currentView == "test2"
+        }}>4択問題</button>
 
-    <button onclick={() => currentView = "test2"} class="btn btn-ghost h-15 md:h-20 grow-1 basis-0">
-        <svg class="h-full" viewBox="0 0 40 40" fill="none">
-            <rect x="15" y="15" width="10" height="10" class={{
-                "fill-sky-500":currentView=="test2",
-                "fill-gray-500": currentView=="memo" || currentView == "edit" || currentView=="test"
-                }} />
-            <line x1="17" y1="20" x2="19" y2="23" class="stroke-1 rounded-2xl stroke-white" stroke-linecap="round"/>
-            <line x1="19" y1="23" x2="23" y2="17" class="stroke-1 rounded-2xl stroke-white" stroke-linecap="round"/>
-            <text x="11" y="35" class="fill-gray-500" font-size="6">つづり</text>
-        </svg>
-    </button>
+    <button onclick={() => currentView = "test2"} class={{
+        "btn btn-ghost font-bold h-full grow-1 basis-0":true,
+        "text-sky-500":currentView=="test2",
+        "text-gray-500": currentView=="test" || currentView == "edit" || currentView == "memo"
+        }}>つづり問題</button>
 </div>
 
