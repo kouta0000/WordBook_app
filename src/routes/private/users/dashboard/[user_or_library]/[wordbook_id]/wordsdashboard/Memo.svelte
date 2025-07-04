1e47937d9be31a2333eb0a2dc226231b962e4a83
kouta0000 <39004878+kouta0000@users.noreply.github.com>
2025-07-05 02:17:09 +0900

    Update Memo.svelte

--- src/routes/private/users/dashboard/[user_or_library]/[wordbook_id]/wordsdashboard/Memo.svelte
+++ src/routes/private/users/dashboard/[user_or_library]/[wordbook_id]/wordsdashboard/Memo.svelte
@@ -127,9 +127,9 @@
             
             {#each wordsc as word,i (word.id)}
             <IntersectionObserver element={cards[i]} on:observe={(e) => {showContents[i]=!showContents[i];shows[i] = false;showPhrases[i]=false}}>
-            <div bind:this={cards[i]} class="w-9/10 h-[20vh] sm:grow flex flex-col justify-center items-start relative">
+            <div bind:this={cards[i]} class="w-9/10 h-[15vh] sm:grow flex flex-col justify-center items-start relative">
                  {#if showContents[i]}
-                 <div class="flex justify-center w-full shadow-lg bg-white shadow-sm rounded-t-xl rounded-r-xl relative">
+                 <div class="flex justify-center w-full h-full shadow-lg bg-white shadow-sm rounded-t-xl rounded-r-xl relative">
                     <div class="grow flex flex-col max-w-9/10 relative">
                         <!--
                         <div class="absolute top-0 right-0 w-1/7">
