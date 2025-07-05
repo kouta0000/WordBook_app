a515d9f5b19293b6bb464796604456040661540e
kota <kukkmathphys1236@gmail.com>
2025-07-05 18:33:04 +0900

    aaa

--- src/routes/private/users/dashboard/[user_or_library]/[wordbook_id]/wordsdashboard/Memo.svelte
+++ src/routes/private/users/dashboard/[user_or_library]/[wordbook_id]/wordsdashboard/Memo.svelte
@@ -192,10 +192,10 @@
                         <div style={parent_style} class="w-full">
                             <p use:fit={{min_size:5, max_size:22}} class="font-sans font-semibold pl-5 pr-1 pt-4 pb-1 text-2xl">{!soundmode? (isFlipped? word.meaning: word.term) : `${word.term[0]}${Array(word.term.length-1).fill("_").join("")}`}</p>
                         </div>
-                        <div class="flex w-full p-1 h-9">
+                        <div class="flex w-full h-7">
                         {#if shows[i]}
                         <div style={parent_style}>
-                        <p use:fit={{min_size:5, max_size:20}} transition:fade={{duration:300}} class="text-gray-500 text-right font-sans pr-2">
+                        <p use:fit={{min_size:5, max_size:15}} transition:fade={{duration:300}} class="text-gray-500 text-right font-sans pr-2">
                         {isFlipped? word.term: word.meaning}
                         </p>
                         </div>
@@ -221,20 +221,23 @@
                 
                 <div class="w-full relative transition-all duration-200 rounded-b-3xl">
                     <div transition:slide class=" w-9/10 rounded-b-3xl bg-linear-to-r {i%2==0? "from-indigo-50 to-indigo-200":"from-teal-50 to-teal-200"} overflow-hidden flex self-start">
-                        <button onclick={() => {showPhrases[i]=true;showPhrases.forEach((v,id)=>{if(id!=i)showPhrases[id]=false});currentviews[i]="synonym"; displays[i] = fetchtext(word.term,"synonym",word.id,false)}} class="btn  btn-sm bg-white/10 text-indigo-800 font-black w-1/3">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                        <button onclick={() => {showPhrases[i]=true;showPhrases.forEach((v,id)=>{if(id!=i)showPhrases[id]=false});currentviews[i]="synonym"; displays[i] = fetchtext(word.term,"synonym",word.id,false)}} class="btn  btn-sm bg-white/10 text-indigo-900 font-semibold w-1/3">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-3">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z" />
-                            </svg>                                  
+                            </svg>
+                            <p>類語</p>                                  
                         </button>
-                        <button onclick={() => {showPhrases[i]=true;showPhrases.forEach((v,id)=>{if(id!=i)showPhrases[id]=false});currentviews[i]="collocation";displays[i] = fetchtext(word.term,"collocation",word.id,false)}} class="btn btn-sm bg-white/10 text-indigo-800 font-black w-1/3">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                        <button onclick={() => {showPhrases[i]=true;showPhrases.forEach((v,id)=>{if(id!=i)showPhrases[id]=false});currentviews[i]="collocation";displays[i] = fetchtext(word.term,"collocation",word.id,false)}} class="btn btn-sm bg-white/10 text-indigo-900 font-semibold w-1/3">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-3">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M13.19 8.688a4.5 4.5 0 0 1 1.242 7.244l-4.5 4.5a4.5 4.5 0 0 1-6.364-6.364l1.757-1.757m13.35-.622 1.757-1.757a4.5 4.5 0 0 0-6.364-6.364l-4.5 4.5a4.5 4.5 0 0 0 1.242 7.244" />
-                            </svg>                                  
+                            </svg> 
+                            <p>フレーズ</p>                                     
                         </button>
-                        <button onclick={() => {showPhrases[i]=true;showPhrases.forEach((v,id)=>{if(id!=i)showPhrases[id]=false});currentviews[i]="sentence";displays[i] = fetchtext(word.term, "sentence", word.id,false)}} class="btn btn-sm bg-white/10 text-indigo-800 font-black  w-1/3">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                        <button onclick={() => {showPhrases[i]=true;showPhrases.forEach((v,id)=>{if(id!=i)showPhrases[id]=false});currentviews[i]="sentence";displays[i] = fetchtext(word.term, "sentence", word.id,false)}} class="btn btn-sm bg-white/10 text-indigo-900 font-semibold w-1/3">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-3">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25ZM6.75 12h.008v.008H6.75V12Zm0 3h.008v.008H6.75V15Zm0 3h.008v.008H6.75V18Z" />
-                            </svg>                                  
+                            </svg> 
+                            <p>短文</p>                                     
                         </button>
                     </div>
                     {#if showPhrases[i]}
